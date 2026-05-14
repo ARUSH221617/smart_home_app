382a8f80fcf6e6c27e787268d1a3ad872e9e16a6
arush <arush221617@gmail.com>
2026-05-14 23:36:49 +0330

    .

--- lib/utils/provider_observer.dart
+++ lib/utils/provider_observer.dart
@@ -3,44 +3,40 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../constants/constants.dart';
 
-class AppObserver extends ProviderObserver {
+final class AppObserver extends ProviderObserver {
   @override
   void didAddProvider(
-    ProviderBase<Object?> provider,
-    Object? value,
-    ProviderContainer container,
-  ) {
+      ProviderObserverContext context,
+      Object? value,
+      ) {
     debugPrint(
-        '${Constants.tag} Provider ${provider.name} was initialized with $value');
+        '${Constants.tag} Provider ${context.provider.name} was initialized with $value');
   }
 
   @override
   void didDisposeProvider(
-    ProviderBase<Object?> provider,
-    ProviderContainer container,
-  ) {
-    debugPrint('${Constants.tag} Provider ${provider.name} was disposed');
+      ProviderObserverContext context,
+      ) {
+    debugPrint('${Constants.tag} Provider ${context.provider.name} was disposed');
   }
 
   @override
   void didUpdateProvider(
-    ProviderBase<Object?> provider,
-    Object? previousValue,
-    Object? newValue,
-    ProviderContainer container,
-  ) {
+      ProviderObserverContext context,
+      Object? previousValue,
+      Object? newValue,
+      ) {
     debugPrint(
-        '${Constants.tag} Provider ${provider.name} updated from $previousValue to $newValue');
+        '${Constants.tag} Provider ${context.provider.name} updated from $previousValue to $newValue');
   }
 
   @override
   void providerDidFail(
-    ProviderBase<Object?> provider,
-    Object error,
-    StackTrace stackTrace,
-    ProviderContainer container,
-  ) {
+      ProviderObserverContext context,
+      Object error,
+      StackTrace stackTrace,
+      ) {
     debugPrint(
-        '${Constants.tag} Provider ${provider.name} threw $error at $stackTrace');
+        '${Constants.tag} Provider ${context.provider.name} threw $error at $stackTrace');
   }
 }
